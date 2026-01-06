1fda9c97df6f9f3007b4f24c5534911b6934cb84
cloudflypeng <cloudflypeng@gmail.com>
2026-01-06 16:10:53 +0800

    feat: 更新背景组件，优化对角线网格背景的透明度和颜色模式支持

--- app/part/Bg.vue
+++ app/part/Bg.vue
@@ -1,17 +1,18 @@
 <template>
   <!-- 对角线网格背景 -->
-  <div class="fixed w-screen h-screen -z-1 pointer-events-none opacity-50" :style="{
+  <div class="fixed w-screen h-screen -z-10 pointer-events-none" :class="isDark ? 'opacity-20' : 'opacity-50'" :style="{
     backgroundImage: `
-          repeating-linear-gradient(45deg, rgba(0, 0, 0, 0.1) 0, rgba(0, 0, 0, 0.1) 1px, transparent 1px, transparent 20px),
-          repeating-linear-gradient(-45deg, rgba(0, 0, 0, 0.1) 0, rgba(0, 0, 0, 0.1) 1px, transparent 1px, transparent 20px)
+          repeating-linear-gradient(45deg, ${isDark ? 'rgba(255, 255, 255, 0.1)' : 'rgba(0, 0, 0, 0.1)'} 0, ${isDark ? 'rgba(255, 255, 255, 0.1)' : 'rgba(0, 0, 0, 0.1)'} 1px, transparent 1px, transparent 20px),
+          repeating-linear-gradient(-45deg, ${isDark ? 'rgba(255, 255, 255, 0.1)' : 'rgba(0, 0, 0, 0.1)'} 0, ${isDark ? 'rgba(255, 255, 255, 0.1)' : 'rgba(0, 0, 0, 0.1)'} 1px, transparent 1px, transparent 20px)
         `,
     backgroundSize: '40px 40px',
   }">
   </div>
 </template>
 
-<style scoped>
-.dark .absolute {
-  opacity: 0.3;
-}
-</style>
+<script setup lang="ts">
+const colorMode = useColorMode()
+const isDark = computed(() => colorMode.value === 'dark')
+</script>
+
+<style scoped></style>
